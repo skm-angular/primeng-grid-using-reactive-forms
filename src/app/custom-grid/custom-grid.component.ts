4cde2ba945cb7f599fefaf92ed204fb4ae99fcef
ssankathali <muddamsetty.santhosh@gmail.com>
2022-01-22 11:27:17 -0500

    completed binding p table to reactive form

--- src/app/custom-grid/custom-grid.component.ts
+++ src/app/custom-grid/custom-grid.component.ts
@@ -1,4 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-custom-grid',
@@ -6,9 +13,41 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./custom-grid.component.scss'],
 })
 export class CustomGridComponent implements OnInit {
+  fg: FormGroup;
   @Input() gridHeaders: any[] = [];
   @Input() data: any[] = [];
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.fg = this.fb.group({});
+    this.createForm();
+  }
 
   ngOnInit(): void {}
+
+  /**
+   * Initializes the Form & by default adds an empty row to the PRIMENG TABLE
+   */
+  private createForm(): void {
+    this.fg = this.fb.group({
+      rows: this.fb.array([this.createTableRow()]),
+    });
+  }
+
+  /**
+   * Returns the FormGroup as a Table Row
+   */
+  private createTableRow(): FormGroup {
+    return this.fb.group({
+      name: new FormControl('awdawd', {
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(50),
+        ],
+      }),
+    });
+  }
+
+  get rows(): FormArray {
+    return this.fg.get('rows') as FormArray;
+  }
 }
