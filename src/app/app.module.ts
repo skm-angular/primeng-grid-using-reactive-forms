e4a06d118c53c1e0628c701cb0e005d402a2de61
ssankathali <muddamsetty.santhosh@gmail.com>
2022-01-23 01:01:39 -0500

    added filter module but it is not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputTextModule } from 'primeng/inputtext';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { TableModule } from 'primeng/table';
-import { CustomGridComponent } from './custom-grid/custom-grid.component';
-import { TooltipModule } from 'primeng/tooltip';
-import { ControlErrorsComponent } from './control-errors/control-errors.component';
+import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+import { AppComponent } from './app.component';
+import { ControlErrorsComponent } from './control-errors/control-errors.component';
+import { CustomGridComponent } from './custom-grid/custom-grid.component';
 @NgModule({
   declarations: [AppComponent, CustomGridComponent, ControlErrorsComponent],
   imports: [
@@ -22,6 +23,7 @@ import { DropdownModule } from 'primeng/dropdown';
     TableModule,
     TooltipModule,
     DropdownModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
